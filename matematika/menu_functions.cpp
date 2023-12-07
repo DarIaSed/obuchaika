0b52890965e540151b8a502f083e3d3f21705013
DariaS <pdaryasedegova@qmail.com>
2023-12-07 20:07:49 +0500

    Create menu main and study

---
+++ matematika/menu_functions.cpp
@@ -0,0 +1,53 @@
+﻿#include <cstdlib>
+#include <iostream>
+
+#include "menu.hpp"
+#include "menu_functions.hpp"
+
+const MAst::MenuItem* MAst::show_menu(const MenuItem* current) {
+	std::cout << "Обучайка приветсвует тебя, мой юный ученк!" << std::endl;
+	for (int i = 1; i < current->children_count; i++) {
+		std::cout << current->children[i]->title << std::endl;
+	}
+	std::cout << current->children[0]->title << std::endl;
+	std::cout << "Обучайка > ";
+
+	int user_input;
+	std::cin >> user_input;
+	std::cout << std::endl;
+	return current->children[user_input];
+}
+
+const MAst::MenuItem* MAst::exit(const MenuItem* current) {
+	std::exit(0);
+}
+
+const MAst::MenuItem* MAst::study_summ(const MenuItem* current) {
+	// TODO
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const MAst::MenuItem* MAst::study_substract(const MenuItem* current) {
+	// TODO
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const MAst::MenuItem* MAst::study_multiply(const MenuItem* current) {
+	// TODO
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const MAst::MenuItem* MAst::study_divide(const MenuItem* current) {
+	// TODO
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent;
+}
+
+const MAst::MenuItem* MAst::study_go_back(const MenuItem* current) {
+	// TODO
+	std::cout << current->title << std::endl << std::endl;
+	return current->parent->parent;
+}
---
+++ matematika/menu_items.cpp
@@ -0,0 +1,51 @@
+﻿#include "menu_items.hpp"
+
+#include <cstddef>
+
+#include "menu_functions.hpp"
+
+const MAst::MenuItem MAst::STUDY_SUMM = {
+	 "1 - Хочу научиться складывать!", MAst::study_summ, &MAst::STUDY
+};
+const MAst::MenuItem MAst::STUDY_SUBSTRACT = {
+	 "2 - Хочу научиться вычитать!", MAst::study_substract, &MAst::STUDY
+};
+const MAst::MenuItem MAst::STUDY_MULTIPLY = {
+	 "3 - Хочу научиться умножать!", MAst::study_multiply, &MAst::STUDY
+};
+const MAst::MenuItem MAst::STUDY_DIVIDE = {
+	 "4 - Хочу научиться делить!", MAst::study_divide, &MAst::STUDY
+};
+const MAst::MenuItem MAst::STUDY_GO_BACK = {
+	 "0 - Выйти в главное меню", MAst::study_go_back, &MAst::STUDY
+};
+
+namespace {
+	const MAst::MenuItem* const study_children[] = {
+		&MAst::STUDY_GO_BACK, 
+		&MAst::STUDY_SUMM,
+		&MAst::STUDY_SUBSTRACT,
+		&MAst::STUDY_MULTIPLY,
+		&MAst::STUDY_DIVIDE,
+	};
+	const int study_size = sizeof(study_children) / sizeof(study_children[0]);
+}
+
+const MAst::MenuItem MAst::STUDY = {
+	"1 - Хочу учиться математике!", MAst::show_menu, &MAst::MAIN, study_children, study_size
+};
+const MAst::MenuItem MAst::EXIT = {
+	"0 - Я лучше полежу...", MAst::exit, &MAst::MAIN
+};
+
+namespace {
+	const MAst::MenuItem* const main_children[] = {
+		&MAst::EXIT,
+		&MAst::STUDY
+	};
+	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
+}
+
+const MAst::MenuItem MAst::MAIN = {
+	nullptr, MAst::show_menu, nullptr, main_children, main_size
+};
